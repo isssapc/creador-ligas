0da423e80b504909ee537b94a254c45f81273699
Ramiro Arechar <ramiro@arechar>
2017-10-27 23:55:13 -0500

    init

---
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatMenuModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatTabsModule,
+  MatListModule,
+  MatExpansionModule,
+  MatSlideToggleModule,
+  MatCardModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDialogModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule,
+  MatGridListModule
+} from "@angular/material";
+
+@NgModule({
+  imports: [
+    MatMenuModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatTabsModule,
+    MatListModule,
+    MatExpansionModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatGridListModule
+  ],
+  exports: [
+    MatMenuModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatTabsModule,
+    MatListModule,
+    MatExpansionModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatGridListModule
+  ]
+})
+export class AngularMaterialModule { }
