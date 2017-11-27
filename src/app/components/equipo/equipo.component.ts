6c38da9e103d1b23034f8f9641f22f44e274a26f
Ramiro Arechar <ramiro@arechar>
2017-11-27 15:30:43 -0600

    create jugador

--- src/app/components/equipo/equipo.component.ts
+++ src/app/components/equipo/equipo.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { EquipoService } from '../../services/equipo.service';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { JugadorService } from '../../services/jugador.service';
+import { CrearJugadorComponent } from '../../dialogos/crear-jugador/crear-jugador.component';
+import { MatDialog, MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-equipo',
@@ -13,13 +15,17 @@ export class EquipoComponent implements OnInit {
   //jugadores: any[];
 
   equipo: any = {};
+  jugador_selected: any = {};
 
   constructor(
     private equipoSrv: EquipoService,
     private jugadorSrv: JugadorService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
   ) { }
+
 
   ngOnInit() {
 
@@ -32,15 +38,43 @@ export class EquipoComponent implements OnInit {
 
   }
 
-  addJugador(nombre) {
-    let id_equipo = this.route.snapshot.paramMap.get("id");
+  createJugador() {
 
-    let jugador = { nombre: nombre, id_equipo: id_equipo };
 
-    this.jugadorSrv.createJugador(jugador)
-      .subscribe(jugador => {
-        this.equipo.jugadores.push(jugador);
-      });
+    let dialogRef = this.dialog.open(CrearJugadorComponent, {
+      data: {
+        id_equipo: this.equipo.id_equipo,
+        jugadores: this.equipo.jugadores
+
+      },
+      width: "500px"
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+      if (result) {
+
+        if (result.error) {
+          this.snackBar.open(result.error, "Cerrar", {
+            duration: 2000
+          });
+        } else {
+          this.snackBar.open("Jugador Creado", "Cerrar", {
+            duration: 2000
+          });
+        }
+
+      }
+
+
+    });
+
+
+
+
+
+
+
 
 
   }
--- src/app/dialogos/crear-jugador/crear-jugador.component.ts
+++ src/app/dialogos/crear-jugador/crear-jugador.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { JugadorService } from '../../services/jugador.service';
 
 @Component({
   selector: 'app-crear-jugador',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-jugador.component.scss']
 })
 export class CrearJugadorComponent implements OnInit {
+  jugador: any = {};
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<CrearJugadorComponent>,
+    private jugadorSrv: JugadorService
+  ) { }
 
   ngOnInit() {
+    this.jugador.id_equipo = this.data.id_equipo;
+  }
+
+  createJugador() {
+
+    this.jugadorSrv.createJugador(this.jugador)
+      .subscribe(jugador => {
+
+        this.data.jugadores.push(jugador);
+        this.dialogRef.close(jugador);
+
+      },
+      (error: any) => {
+        this.dialogRef.close({ error: "ERROR. Verifique sus datos o compruebe su conexión" });
+      });
+
   }
 
 }
